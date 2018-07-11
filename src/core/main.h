43ee6f0c8392f4607dcec7bed7130a2925bf84b6
HOWARD LARRISON <development@daybreak.coffee>
2018-07-12 00:02:59 +0700

    Update main.h

--- src/core/main.h
+++ src/core/main.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2010 Satoshi Nakamoto.
 // Copyright (c) 2010-2018 The Bitcoin developers
 // Original code was distributed under the MIT software license.
 // Copyright (c) 2014-2018 TEDLab Sciences Ltd
